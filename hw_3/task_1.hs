a94ce26edd050e4a512f5c3b1da7ae8870492df5
nikonepo <nikolainepo@gmail.com>
2025-03-31 17:41:40 +0300

    Add assert

---
+++ hw_3/task_1.hs
@@ -0,0 +1,17 @@
+import Debug.Trace (assert)
+
+composition :: (b -> c, a -> b) -> a -> c
+composition (g, f) = g . f
+
+composeFuncs :: [Double -> Double] -> Int -> Int
+composeFuncs fs n = round $ (foldr (.) id fs) (fromIntegral n)
+
+main :: IO ()
+main = do
+    let test1 = composition ((+1), (*2)) 3
+    let _ = assert (test1 == 7) (return ())
+
+    let test2 = composeFuncs [(*2), (+3)] 4
+    let _ = assert (test2 == 14) (return ())
+
+    putStrLn "All tests passed."
