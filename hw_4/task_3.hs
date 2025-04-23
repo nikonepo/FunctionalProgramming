283a06d14c673d02bef93e55a57b9ed3144b7b35
nikonepo <nikolainepo@gmail.com>
2025-04-23 23:40:05 +0300

    Fix

--- hw_4/task_3.hs
+++ hw_4/task_3.hs
@@ -12,7 +12,7 @@ assert _     x = x
 main :: IO ()
 main = do
     let s123 = segs [1,2,3]
-    let _ = assert (s123 == [[1],[1,2],[1,2,3],[2],[2,3],[3]]) ()
+    let _ = assert (s123 == [[1],[1,2],[1,2],[2],[2,3],[3]]) ()
 
     let segH = segs "hello"
         expectedOrder = [ "h","he","hel","hell","hello", "e","el","ell","ello", "l","ll","llo", "l","lo", "o"]
