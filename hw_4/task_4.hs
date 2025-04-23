1805f940548627fac790576539d97f06753abbe5
nikonepo <nikolainepo@gmail.com>
2025-04-23 23:50:38 +0300

    Task 4

---
+++ hw_4/task_4.hs
@@ -0,0 +1,19 @@
+nrem :: Int -> [a] -> [a]
+nrem n a
+    | n == 0 = a
+    | otherwise [ x | (i,x) <- zip a [1..], i `mod` n /= 0 ]
+
+--
+assert :: Bool -> a -> a
+assert False x = error "assertion failed!"
+assert _     x = x
+--
+
+main :: IO ()
+main = do
+    let _ = assert (nrem 3 [1,2,3,4,5,6,7] == [1,2,4,5,7]) ()
+    let _ = assert (nrem 1 [10,20,30] == []) ()
+    let _ = assert (nrem 10 [1..5] == [1,2,3,4,5]) ()
+    let _ = assert (nrem 2 "abcdef" == "ace") ()
+
+    putStrLn "All tests passed."
