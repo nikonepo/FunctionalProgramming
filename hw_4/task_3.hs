1383d3790b267bc9440b04382b8f95afd94a8a2f
nikonepo <nikolainepo@gmail.com>
2025-04-23 23:39:01 +0300

    Fix

--- hw_4/task_3.hs
+++ hw_4/task_3.hs
@@ -12,10 +12,10 @@ assert _     x = x
 main :: IO ()
 main = do
     let s123 = segs [1,2,3]
-    assert (s123 == [[1],[1,2],[1,2,3],[2],[2,3],[3]]) "segs [1,2,3]"
+    let _ = assert (s123 == [[1],[1,2],[1,2,3],[2],[2,3],[3]]) ()
 
     let segH = segs "hello"
         expectedOrder = [ "h","he","hel","hell","hello", "e","el","ell","ello", "l","ll","llo", "l","lo", "o"]
-    assert (segH == expectedOrder) "segs \"hello\" exact order"
+    let _ = assert (segH == expectedOrder) ()
 
     putStrLn "All tests passed."
